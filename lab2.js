1b6fbb2aad1902f08aa79cc0b8188831268c4422
Eyobed <88844574+eyuu1@users.noreply.github.com>
2022-02-05 02:13:02 +0300

    Add files via upload
    //added on Registration part

---
+++ lab2.js
@@ -0,0 +1,12 @@
+
+function myfunction(){
+    alert("the audio will start within seconds please wait...");
+};
+
+function button(){
+    alert("you will be directed to sign in page");
+};
+
+function onkey(){
+    alert("you pressed a number");
+};
